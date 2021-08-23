409b9979ccf4dd3460b4aca78c8a70f4871144c5
Toan Nguyen <nguyentoandev@gmail.com>
2021-08-23 09:02:28 +0700

    feat: publish a database (WIP)

--- lib/amazon-cdk-practice-rds-stack.ts
+++ lib/amazon-cdk-practice-rds-stack.ts
@@ -1,5 +1,6 @@
 import { App, Duration, Stack, StackProps } from '@aws-cdk/core'
 import {
+  SecurityGroup,
   InstanceType,
   InstanceClass,
   InstanceSize,
@@ -13,21 +14,27 @@ import {
   StorageType,
 } from '@aws-cdk/aws-rds'
 
+// WIP
 export class AmazonCdkPracticeRDSStack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    // TODO: Add a policy to limit IP Address which can access RDS
+    // TODO: Add a rule in security group to limit IP Address which can access RDS
     const vpc = new Vpc(this, 'CDKPracticeVpcForRDS')
-
-    const instance = new DatabaseInstance(this, 'CDKPracticeInstance', {
-      engine: DatabaseInstanceEngine.postgres({ version: PostgresEngineVersion.VER_13_3 }),
-      instanceType: InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.NANO),
-      storageType: StorageType.STANDARD,
-      credentials: Credentials.fromGeneratedSecret('cdk-practice-rds'),
+    const securityGroup = new SecurityGroup(this, 'CDKPracticeRDSSecurityGroup', {
       vpc,
-      allocatedStorage: 20, // Minimum is 20 GiB.
-      backupRetention: Duration.days(7),
+    })
+
+    const database = new DatabaseInstance(this, 'CDKPracticeInstance', {
+      engine: DatabaseInstanceEngine.postgres({ version: PostgresEngineVersion.VER_13_3 }),
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      storageType: StorageType.STANDARD,
+      credentials: Credentials.fromGeneratedSecret('rds_developer1'),
+      vpc,
+      allocatedStorage: 20,
+      iamAuthentication: false,
+      publiclyAccessible: true,
+      backupRetention: Duration.days(0),
     })
   }
 }
